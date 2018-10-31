eb16057108a46e82abb985f3cb1fa1c90e56c503
Jon Nangle <jon.nangle@ft.com>
2018-10-31 10:12:05 +0000

    Add tests

--- test_aws.py
+++ test_aws.py
@@ -1,10 +1,35 @@
 import boto3
 
-"""
-How do we test this code?
-"""
 def list_zip_files(bucket='mybucket'):
     s3 = boto3.resource('s3')
     s3bucket = s3.Bucket(bucket)
     zips = [f for f in s3bucket.objects.all() if f.key.endswith('zip')]
     return zips
+
+from mock import patch, Mock
+
+@patch('boto3.resource')
+def test_list_keys_with_default_bucket(mock):
+    mock.return_value.Bucket.return_value.objects.all.return_value = [Mock(key='file1.zip'), Mock(key='file2.zip'), Mock(key='file3.zip')]
+
+    ziplist = list_zip_files()
+    mock.assert_called_with('s3')
+    mock().Bucket.assert_called_with('mybucket')
+    assert len(ziplist) == 3
+
+@patch('boto3.resource')
+def test_list_keys_with_default_bucket_finds_zipfiles(mock):
+    mock.return_value.Bucket.return_value.objects.all.return_value = [Mock(key='file1.txt'), Mock(key='file2.png'), Mock(key='file3.zip')]
+
+    ziplist = list_zip_files()
+    mock.assert_called_with('s3')
+    mock().Bucket.assert_called_with('mybucket')
+    assert len(ziplist) == 1
+
+@patch('boto3.resource')
+def test_list_keys_with_bucket_name_calls_correct_bucket(mock):
+
+    ziplist = list_zip_files(bucket='someotherbucket')
+
+    mock.assert_called_with('s3')
+    mock().Bucket.assert_called_with('someotherbucket')
